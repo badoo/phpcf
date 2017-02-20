f09fc4254ce000f580090b07608d88b5082b3fb7
Oleg Efimov <efimovov@gmail.com>
2017-02-20 12:19:06 +0300

    Add test for #1.

---
+++ phpcf-src/test/original/mixed-content.php
@@ -0,0 +1,23 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetPageProperty(
+    "title",
+    "TItile"
+);
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
+$APPLICATION->SetTitle("Index");
+?>
+<div class="row">
+    <div class="col-xs-6 col-sm-4 index_block">
+        <div class="title_index_block"><span>menu</span></div>
+        <?php
+        $APPLICATION->IncludeFile(
+            $APPLICATION->GetCurDir() . "/include/include.php",
+            Array(),
+            Array(
+                "MODE"      => "html",
+                "NAME"      => "Content",
+            )
+        );
+        ?>
+    </div>
